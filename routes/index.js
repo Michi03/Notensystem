868fef342ec83388316f66f9de5747d54bde4042
Michi03 <mic.michael@gmx.de>
2024-05-12 20:08:12 +0200

    prepare next lesson

--- routes/index.js
+++ routes/index.js
@@ -9,57 +9,35 @@ function setDB(dbConnection) {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.getStudents( (students) => {
-    console.log(students);
-    db.getTeachers( (teachers) => {
-      console.log(teachers);
-      res.render('index', {'students': students, 'teachers': teachers});
-    });
-  });
+  res.render('index', {});
 });
 
 /* GET DB table. */
 router.get('/users', function(req, res, next) {
-  db.selectAllRows('User', (rows) => {
+  db.selectAllRows('Users', (rows) => {
     console.log(rows);
-    res.render('table', {'table': 'User', 'keys': Object.keys(rows[0]), 'rows': rows});
+    res.render('table', {'table': 'Users', 'keys': Object.keys(rows[0]), 'rows': rows});
   });
 });
 
 /* GET admin panel. */
 router.get('/admin', function(req, res, next) {
-  db.selectAllRows('User', (users) => {
-    console.log(users);
-    db.selectAllRows('Class', (classes) => {
-      console.log(classes);
-      res.render('admin', {'users': users, 'classes': classes});
-    });
-  });
+  res.render('admin', {});
 });
 
 /* GET admin settings for user. */
-router.get('/user', function(req, res, next) {
-  db.getUser(req.query.id, (user) => {
-    return 'TODO';
-  });
-});
+// router.get('/user', function(req, res, next) {
+//   res.render('user', {});
+// });
 
 /* GET student page. */
 router.get('/student', function(req, res, next) {
-  db.getStudent(req.query.id, (user) => {
-    console.log(user);
-    db.getStudentGrades(req.query.id, (subjects) => {
-      console.log(JSON.stringify(subjects));
-      for (let i = 0; i < subjects.length; i++)
-        subjects[i]['average'] = util.printGrade(util.computeAverageGrade(subjects[i]['grades']));
-      res.render('student', {'user': user, 'subjects': subjects});
-    });
-  });
+  res.render('student', {});
 });
 
 /* GET teacher page. */
 router.get('/teacher', function(req, res, next) {
-  res.render('teacher');
+  res.render('teacher', {});
 });
 
 module.exports = { router, setDB };
